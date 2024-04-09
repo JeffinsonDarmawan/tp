0afbf634ae5b2ab8fb2d1fe287b39153edd21e15
LAPTOP-6F19PDN9\Ijaaz <ijaaz2308@gmail.com>
2024-04-09 16:39:21 +0800

    Uncomment code in parser

--- src/main/java/florizz/core/Parser.java
+++ src/main/java/florizz/core/Parser.java
@@ -145,9 +145,9 @@ public class Parser {
             outputs[0] = FuzzyLogic.detectItem(trimmedInput.toLowerCase());
         }
 
-        /*if (firstWhitespace == -1 && (outputs[0].equals("save"))) {
+        if (firstWhitespace == -1 && (outputs[0].equals("save"))) {
             throw new FlorizzException("Please specify which bouquet you are saving!");
-        }*/
+        }
 
         return outputs;
     }
